63924c63ee59a7330a142aaafc3726d245639cd7
Lê Quý Hoàng Tâm <127521514+ellie2222222@users.noreply.github.com>
2024-10-29 15:57:13 +0700

    adjust cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["*"],
+    origin: "*",
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
